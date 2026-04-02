934e49ba06aeff16fc3b5e5bdd317a051b3165dd
turanlz <turanhasan013@gmail.com>
2026-04-03 03:29:14 +0400

    feat: initial project setup

---
+++ src/components/layout/sidebar-left/sidebar-left.tsx
@@ -0,0 +1,7 @@
+export const SidebarLeft = () => {
+  return (
+    <aside className="w-[280px] h-screen fixed left-0 top-0 border-r border-border bg-background flex flex-col p-4">
+      <div className="text-primary font-bold text-xl mb-8">ONYX (Left)</div>
+    </aside>
+  );
+};
---
+++ src/components/layout/sidebar-right/sidebar-right.tsx
@@ -0,0 +1,7 @@
+export const SidebarRight = () => {
+  return (
+    <aside className="w-[320px] h-screen fixed right-0 top-0 border-l border-border bg-background flex flex-col p-4">
+      <div className="text-text-muted font-bold text-sm mb-4">Friends (Right)</div>
+    </aside>
+  );
+};
